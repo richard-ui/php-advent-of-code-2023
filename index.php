f24920e12a7d12ba462c1bb9cc5d4f7d3ab7ad3f
Richard Jones <richard-jones96@hotmail.co.uk>
2024-04-07 11:10:48 +0100

    I have Implemented part 2 functionality within dayOne class and put piece of reusble code from part 1 function inside its own to prevent duplication and to follow the common DRY principle. so it can be called in both parts 1 and 2

--- index.php
+++ index.php
@@ -1,92 +1,87 @@
 <?php
 
-// echo "Welcome to Day One - Advents of Code!";
+    echo "Welcome to Day One - Advents of Code!\n";
 
-class InputList extends dayOne {
+    class InputList extends dayOne {
 
-    public $inputTxt = "";
+        public $inputTxt = "";
 
-    public function __construct($inputTxt) {
-        $this->inputTxt = $inputTxt;
+        public function __construct($inputTxt) {
+            $this->inputTxt = $inputTxt;
+        }
+
+        public function getInput() {
+            $contents = file_get_contents($this->inputTxt);
+            $input = preg_split('/\r\n|\r|\n/', $contents);
+            return $input;
+        }
     }
 
-    public function getInput() {
-        $contents = file_get_contents($this->inputTxt);
-        $input = preg_split('/\r\n|\r|\n/', $contents);
-        return $input;
-    }
-}
+    class dayOne {
 
-class dayOne {
+        public $total = 0;
 
-    public $total = 0;
+        public function __construct() {
+            $this->total = $total;
+        }
 
-    public function __construct($total) {
-        $this->total = $total;
-    }
+        public function getPart1() {
+            $total = $this->total;
+            $input = $this->getInput();
 
-    public function getTotal() {
-        // $total = $this->total;
-        // return $total;
+            foreach ($input as $line) {
+                $total = $this->calculateTotal($total, $line);
+            }
+
+            return $total;
+        }
+
+        public function getPart2() {
+            $total = $this->total;
+            $input = $this->getInput();
+
+            $wordNumbers = [
+                'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'
+            ];
+
+            foreach ($input as $line) {
+                $characters = str_split($line);
+                $newLine = '';
+                
+                foreach ($characters as $character) {// get first character
+                    // echo $character;// two1nine
+                    $newLine .= $character;
         
-        foreach ($input as $line) {
-            $characters = str_split($line); 
+                    foreach ($wordNumbers as $index => $word) {
+                        // echo $index + 1 . $character . "\n";
+                        // echo $newLine . "\n";
+                        // exit();
+                        // $searchVal, $replaceVal, $subjectVal.
+                        $newLine = str_replace($word, $index + 1 . $character, $newLine);// two 2
+                        // echo $newLine . "\n"; // t two matches two then it will stop and go to next one.
+                    }
+                    // echo $newLine . "\n";
+                }
+                // echo $newLine; // 2o19e. extract 1 and last numbers from this - 29
+                // exit();
+                $total = $this->calculateTotal($total, $newLine);
+            }
+            return $total;
+        }
+
+        public function calculateTotal($total, $newLine){
+            $characters = str_split($newLine); 
             $digits = array_values(array_filter($characters, "is_numeric"));
     
             $length = count($digits);
             $firstNumber = $digits[0];
             $lastNumber = $digits[$length - 1];
             $total += $firstNumber . $lastNumber;
+            return $total;
         }
-    
-        return $total; 
-
     }
-}
 
     $input = new InputList("input.txt");
-    echo $input->getTotal();
-    exit();
-
-    $input = new InputList("input.txt");
-    echo $input->getInput();
-    foreach($input->getInput() as $line) {
-        echo $line;
-        echo "\n";
-    }
-    exit();
-
-// $input = [
-    // '1abc2',
-    // 'pqr3stu8vwx',
-    // 'a1b2c3d4e5f',
-    // 'treb7uchet',
-// ];
-function part1()
-{
-    // Get string values by line.
-    $contents = file_get_contents("input.txt");
-    $input = preg_split('/\r\n|\r|\n/', $contents);
-    $total = 0;
-
-    foreach ($input as $line) {
-        $characters = str_split($line);
-        // var_dump($characters); 
-        $digits = array_values(array_filter($characters, "is_numeric"));
-        // $digits = array_values($digits);
-        // $digits = array_values(array_filter($characters, fn (string $character) => is_numeric($character)));
-        // $var = preg_match_all('/\d/', $line, $matches);
-        // echo $b;
-
-        $length = count($digits);
-        $firstNumber = $digits[0];
-        $lastNumber = $digits[$length - 1];
-        $total += $firstNumber . $lastNumber;
-    }
-
-    return $total;
-}
-
-echo part1();
-
+    echo "Part 1: " . $input->getPart1() . "\n";
+    echo "Part 2: " . $input->getPart2();
 ?>
